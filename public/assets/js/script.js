6a57b786177526146da2a6c2e7b963f4daabf444
Perrine <perrine.morsch@gmail.com>
2022-10-26 11:25:24 +0200

    Ajout d'une nouvelle page et d'un peu de JS

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,26 @@
-button.onclick = () => {
-    window.open('https://javascript.info');
-  };
+document.getElementById("reservation").addEventListener("click", myFunction);
+
+function myFunction() {
+window.alert("Redirection vers un formulaire de réservation")
+}
+
+
+document.getElementById("inscription").addEventListener("click", myFunction2);
+
+function myFunction2() {
+    window.alert("Redirection vers un formulaire d'inscription")
+    }
+
+
+document.getElementById("connection").addEventListener("click", myFunction3);
+
+function myFunction3() {
+    window.alert("Redirection vers un formulaire de connection")
+    document.location.href="oops.html"
+    }
+
+document.getElementById("back").addEventListener("click", myFunction4);
+
+function myFunction4() {
+    document.location.href="index.html"
+    }
